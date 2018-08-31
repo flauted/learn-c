2cea20ebc74aeced66925a3d060ac3e512474365
Dylan F <dylan.flaute@gmail.com>
2018-08-30 20:40:57 -0400

    [ex32] stateless tests.

--- liblcthw/tests/list_tests.c
+++ liblcthw/tests/list_tests.c
@@ -2,12 +2,14 @@
 #include <lcthw/list.h>
 #include <assert.h>
 
-static List *list = NULL;
+// static List* list = NULL;
 char* test1 = "test1 data";
 char* test2 = "test2 data";
 char* test3 = "test3 data";
 
 char* test_create() {
+    static List* list = NULL;
+
     list = List_create();
     mu_assert(list != NULL, "Failed to create list.");
 
@@ -15,12 +17,18 @@ char* test_create() {
 }
 
 char* test_destroy() {
+    static List* list = NULL;
+    list = List_create();
+
     List_clear_destroy(list);
 
     return NULL;
 }
 
 char* test_push_pop() {
+    static List* list = NULL;
+    list = List_create();
+
     List_push(list, test1);
     mu_assert(List_last(list) == test1, "Wrong last value.");
 
@@ -45,6 +53,9 @@ char* test_push_pop() {
 }
 
 char* test_unshift() {
+    static List* list = NULL;
+    list = List_create();
+
     List_unshift(list, test1);
     mu_assert(List_first(list) == test1, "Wrong first value.");
 
@@ -61,22 +72,35 @@ char* test_unshift() {
 char* test_remove() {
     // we only need to test the middle remove case since push/shift
     // already tests the other cases
+    static List* list = NULL;
+    list = List_create();
 
+    List_push(list, test1);
+    List_push(list, test2);
+    List_push(list, test3);
     char* val = List_remove(list, list->first->next);
     mu_assert(val == test2, "Wrong removed element.");
     mu_assert(List_count(list) == 2, "Wrong count after remove.");
-    mu_assert(List_first(list) == test3, "Wrong first value.");
-    mu_assert(List_last(list) == test1, "Wrong first value.");
+    mu_assert(List_first(list) == test1, "Wrong first value.");
+    mu_assert(List_last(list) == test3, "Wrong first value.");
 
     return NULL;
 }
 
 char* test_shift() {
+    static List* list = NULL;
+    list = List_create();
+    List_push(list, test1);
+    List_push(list, test2);
+    List_push(list, test3);
+
     mu_assert(List_count(list) != 0, "Wrong count before shift.");
     char* val = List_shift(list);
-    mu_assert(val == test3, "Wrong value on shift.");
-    val = List_shift(list);
     mu_assert(val == test1, "Wrong value on shift.");
+    val = List_shift(list);
+    mu_assert(val == test2, "Wrong value on shift.");
+    val = List_shift(list);
+    mu_assert(val == test3, "Wrong value on shift.");
     mu_assert(List_count(list) == 0, "Wrong count after shift.");
 
     return NULL;
